230c5740d43904f20878a81f0244b5efa5f80ff2
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-01-15 19:34:42 -0400

    feat: create securityFilter

---
+++ src/main/java/br/com/teste/todolist/infra/security/SecurityFilter.java
@@ -0,0 +1,62 @@
+package br.com.teste.todolist.infra.security;
+
+import br.com.teste.todolist.exceptions.Exception404;
+import br.com.teste.todolist.module.User;
+import br.com.teste.todolist.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public SecurityFilter (TokenService tokenService, UserRepository userRepository) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String token = this.recoverToken(request);
+        String login = this.tokenService.validateToken(token);
+
+        if (login != null) {
+            User user = userRepository.findByEmail(login).orElseThrow(() -> new Exception404("Usuário não encontrado"));
+
+            List<SimpleGrantedAuthority> authorities = Collections
+                    .singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user,
+                    null, authorities);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String authHerader = request.getHeader("Authorization");
+
+        if (authHerader == null) return null;
+
+        return authHerader.replace("Bearer", "");
+    }
+}
