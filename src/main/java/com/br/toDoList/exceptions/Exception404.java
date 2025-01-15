057d3e72ccf425ab963f8d7d0a2bed57d355ff2c
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-01-15 09:36:00 -0400

    feat: create exceptions

---
+++ src/main/java/com/br/toDoList/exceptions/Exception404.java
@@ -0,0 +1,12 @@
+package com.br.toDoList.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class Exception404 extends RuntimeException {
+    public Exception404(String message) {
+        super(message);
+    }
+}
+
