b7888812650830120e1acdd11f41cfd84130fe2d
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-01-15 19:14:53 -0400

    feat: create user entity

---
+++ src/main/java/br/com/teste/todolist/module/User.java
@@ -0,0 +1,55 @@
+package br.com.teste.todolist.module;
+
+import jakarta.persistence.*;
+
+@Table(name = "users")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
