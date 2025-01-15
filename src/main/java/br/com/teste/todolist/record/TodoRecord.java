aff8f33af431b537b0536d7c91cdc88023fa1fe3
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-01-15 15:35:35 -0400

    fix: ajuste validacao

--- src/main/java/br/com/teste/todolist/record/TodoRecord.java
+++ src/main/java/br/com/teste/todolist/record/TodoRecord.java
@@ -2,15 +2,17 @@ package br.com.teste.todolist.record;
 
 import br.com.teste.todolist.module.Todo;
 import br.com.teste.todolist.module.enuns.Status;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 public record TodoRecord(Long id,
-                         String title,
+                         @NotBlank(message = "Título não pode estar em branco") String title,
                          String description,
-                         Status status,
+                         @NotNull(message = "O status não pode ser nulo") Status status,
                          LocalDate creationDate,
-                         LocalDate deadline) {
+                         @NotNull(message = "O prazo não pode ser nulo") LocalDate deadline) {
 
     public static TodoRecord toDto(Todo todo) {
         return new TodoRecord(
