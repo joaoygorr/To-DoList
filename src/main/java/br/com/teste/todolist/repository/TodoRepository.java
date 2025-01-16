1315bba270ea952e43a36640e6171dd09b7673d0
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-01-16 19:17:09 -0400

    fix: created new searchs

--- src/main/java/br/com/teste/todolist/repository/TodoRepository.java
+++ src/main/java/br/com/teste/todolist/repository/TodoRepository.java
@@ -1,9 +1,12 @@
 package br.com.teste.todolist.repository;
 
 import br.com.teste.todolist.module.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+    Page<Todo> findByUsuarioName(Pageable pageable, String name);
 }
