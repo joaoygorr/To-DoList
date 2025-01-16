6ea0fb10cd1a0ab603d83d81bbc48f3b5491f642
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-01-16 17:00:38 -0400

    fix: libera swagger

--- src/main/java/br/com/teste/todolist/infra/security/config/SecurityConfig.java
+++ src/main/java/br/com/teste/todolist/infra/security/config/SecurityConfig.java
@@ -37,6 +37,13 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(
+                                "/v1/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
