5ba4c4bdd88a47272d3434c2cf82b16cdc24a3a8
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-01-17 14:26:57 -0400

    feat: create contructor

--- src/main/java/br/com/teste/todolist/module/User.java
+++ src/main/java/br/com/teste/todolist/module/User.java
@@ -30,6 +30,13 @@ public class User {
                 '}';
     }
 
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
     public String getPassword() {
         return password;
     }
