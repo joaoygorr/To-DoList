bf69ad45b7d4d9f8bcb3f8a35a20be2df37598d9
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2025-01-18 11:46:58 -0400

    fix: atualiza teste

--- src/test/java/br/com/teste/todolist/service/user/UserServiceImplTest.java
+++ src/test/java/br/com/teste/todolist/service/user/UserServiceImplTest.java
@@ -3,17 +3,15 @@ package br.com.teste.todolist.service.user;
 import br.com.teste.todolist.exceptions.Exception401;
 import br.com.teste.todolist.infra.security.service.TokenService;
 import br.com.teste.todolist.module.User;
-import br.com.teste.todolist.record.login.LoginRecord;
-import br.com.teste.todolist.record.login.RegisterRequestRecord;
 import br.com.teste.todolist.record.login.ResponseRecord;
 import br.com.teste.todolist.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,8 +20,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class UserServiceImplTest {
 
@@ -40,10 +37,6 @@ class UserServiceImplTest {
     @InjectMocks
     private UserServiceImpl userService;
 
-    private RegisterRequestRecord defaultRequest;
-
-    private LoginRecord loginRecord;
-
     private User user;
 
     @BeforeEach
@@ -51,65 +44,71 @@ class UserServiceImplTest {
         MockitoAnnotations.openMocks(this);
         userService = new UserServiceImpl(userRepository, tokenService, passwordEncoder);
 
-        defaultRequest = new RegisterRequestRecord("souza", "souza@gmail.com", "123456");
-        loginRecord = new LoginRecord("souza@gmail.com", "123456");
         user = new User(1L, "souza", "souza@gmail.com", "123456");
     }
 
     @Test
     @DisplayName("Usuário cadastrado com sucesso")
     void registerSuccess() {
-        when(userRepository.findByEmail(defaultRequest.email())).thenReturn(Optional.empty());
-        when(passwordEncoder.encode(defaultRequest.password())).thenReturn("encodedPassword");
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.empty());
+        when(passwordEncoder.encode(user.getPassword())).thenReturn("encodedPassword");
         when(tokenService.generateToken(any(User.class))).thenReturn("generatedToken");
 
-        ResponseRecord response = userService.register(defaultRequest);
+        ResponseRecord response = userService.register(user);
 
         assertNotNull(response);
         assertEquals("souza", response.name());
         assertEquals("generatedToken", response.token());
-        verify(userRepository).save(any(User.class));
+
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+        verify(userRepository).save(userCaptor.capture());
+
+        User savedUser = userCaptor.getValue();
+
+        assertEquals("souza", savedUser.getName());
+        assertEquals("souza@gmail.com", savedUser.getEmail());
+        assertEquals("encodedPassword", savedUser.getPassword());
     }
 
     @Test
     @DisplayName("Erro ao tentar registrar usuário com credenciais inválidas")
     void registerInvalidCredentialsThrowsException() {
-        when(userRepository.findByEmail(defaultRequest.email()))
+        when(userRepository.findByEmail(user.getEmail()))
                 .thenReturn(Optional.of(new User(1L, "souza", " ", " ")));
 
-        Exception401 exception = assertThrows(Exception401.class, () -> userService.register(defaultRequest));
+        Exception401 exception = assertThrows(Exception401.class, () -> userService.register(user));
         assertEquals("Credenciais inválidas", exception.getMessage());
 
-        verify(userRepository, Mockito.never()).save(any(User.class));
+        verify(userRepository, never()).save(any(User.class));
     }
 
     @Test
     @DisplayName("Login feito com sucesso")
     void loginSuccess() {
-        when(userRepository.findByEmail(loginRecord.email())).thenReturn(Optional.of(user));
-        when(passwordEncoder.matches(loginRecord.password(), user.getPassword())).thenReturn(true);
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(user.getPassword(), user.getPassword())).thenReturn(true);
         when(tokenService.generateToken(user)).thenReturn("generatedToken");
 
-        ResponseRecord response = userService.login(loginRecord);
+        ResponseRecord response = userService.login(user);
 
         assertNotNull(response);
         assertEquals("souza", response.name());
         assertEquals("generatedToken", response.token());
 
-        verify(userRepository).findByEmail(loginRecord.email());
+        verify(userRepository).findByEmail(user.getEmail());
     }
 
     @Test
     @DisplayName("Login com credenciais inválidas")
     void loginInvalidCredentialsThrowsException() {
-        when(userRepository.findByEmail(loginRecord.email())).thenReturn(Optional.of(user));
-        when(passwordEncoder.matches(loginRecord.password(), user.getPassword())).thenReturn(false);
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(user.getPassword(), user.getPassword())).thenReturn(false);
 
-        Exception401 exception401 = assertThrows(Exception401.class, () -> userService.login(loginRecord));
+        Exception401 exception401 = assertThrows(Exception401.class, () -> userService.login(user));
 
         assertEquals("Credenciais inválidas", exception401.getMessage());
 
-        verify(userRepository).findByEmail(loginRecord.email());
-        verify(passwordEncoder).matches(loginRecord.password(), user.getPassword());
+        verify(userRepository).findByEmail(user.getEmail());
+        verify(passwordEncoder).matches(user.getPassword(), user.getPassword());
     }
 }
